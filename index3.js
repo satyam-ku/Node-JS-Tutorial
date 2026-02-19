7d3d0fa64b71da010d297915bcac259fde0052fd
satyam-ku <rajsatyam2005@gmail.com>
2026-02-19 13:46:03 +0530

    add sections of pages

--- index3.js
+++ index3.js
@@ -13,3 +13,12 @@ app.get("/home",(request,response) => {
     console.log(request.url)
     response.send("Response from server for /home .")
 })
+
+app.get("/home/page1",(request,response) => {
+    console.log(request.url)
+    console.log(request.method)
+     console.log(request.body)
+      console.log(request.header)
+       console.log(request.headers.authorization)
+    response.send("Response from server for /home/page1.")
+})
