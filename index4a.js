51f2561432a5a0ea2be36cec246a8a2f725af69e
satyam-ku <rajsatyam2005@gmail.com>
2026-02-19 13:51:17 +0530

    add  practice

--- index4a.js
+++ index4a.js
@@ -14,7 +14,7 @@ app.get("/:num1/:num2", (request,response) => {
 app.get("/x/:num1/y/:num2/z/:num3", (request,response) => {
     const num1 = Number(request.params.num1);
     const num2 = Number(request.params.num2);
-    const num3 = Number(request.params.num2);
+    const num3 = Number(request.params.num3);
 
     response.send((num1 + num2 + num3).toString());
 })
