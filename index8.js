87b75fea4a999ca08ad8d7e56d14c908501b78d2
satyam-ku <rajsatyam2005@gmail.com>
2026-02-20 09:49:25 +0530

    add delete,post

--- index8.js
+++ index8.js
@@ -1,10 +1,11 @@
 import express, { request, response } from "express";
+import { use } from "react";
 const app = express();
-app.listen(8080, () => {
+app.listen(8081, () => {
     console.log("Server Started....")
 })
 
-
+app.use(express.json())
 const users = [
     {id:1,name: "avi",
     email: "avi@mail.com",
@@ -19,9 +20,28 @@ const users = [
     role: "Student" }
 ]
 
+
+app.get("/",(request,response) => {  
+    response.json(users);
+})
+
+app.post("/",(request,response) => {
+    const user = request.body;
+    users.push(user)
+   response.json(users);
+})
+
 app.get("/:id",(request,response) => {
     const user = users.find((user) => user.id === Number(request.params.id));
     if(user){
-        response.send(user);
+        response.json(user);
     }
 })  
+
+app.delete("/:id",(request,response) => {
+    const user = users.find((user) => user.id === Number(request.params.id));
+    if(user){
+        users.pop(user)
+        response.json(users)
+    }
+})
