1f22f2b786220d033c1ddd46f2149f90c8cd9060
satyam-ku <rajsatyam2005@gmail.com>
2026-02-23 14:19:51 +0530

    add authorization

--- index16.js
+++ index16.js
@@ -1,66 +1,50 @@
 import express from "express";
 import bcrypt from "bcrypt";
-import crypto from "crypto";
+import jwt from "jsonwebtoken";
+const secretKey = "mysecretkey";
+
 const app = express();
-
+app.listen(8080, () => {
+  console.log("server is starting");
+});
+const middleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader)
+    return res.status(401).json({ message: "Authorization header missing" });
+  const token = authHeader.split(" ")[1];
+  try {
+    jwt.verify(token, secretKey);
+    console.log("middleware executed");
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
 app.use(express.json());
-
-app.listen(8080, () => {console.log("Server Started....")});
-
 const users = [];
-
-app.post("/signup", async (req, res) => {
+app.post("/signup", (req, res) => {
   const body = req.body;
-  const hashPassword = await bcrypt.hash(body.password, 10);
-  body.password = hashPassword;
+  const hasPassword = bcrypt.hashSync(body.password, 10);
+  body.password = hasPassword;
   users.push(body);
   res.json(users);
 });
-
-
-function auth(req, res, next) {
-  const token = req.headers.authorization;
-
-  if (!token) {
-    return res.status(403).json({ message: "No token provided" });
-  }
-
-  const user = users.find((u) => u.token === token);
-
-  if (!user) {
-    return res.status(401).json({ message: "Invalid Token" });
-  }
-
-  req.user = user;
-  next(); 
-}
-
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
+  const user = users.find((u) => u.email === email);
 
-  const found = users.find((user) => user.email === email);
+  const ckPassword = await bcrypt.compare(password, user.password);
 
-  if (!found) {
-    return res.status(401).json({ message: "User not found" });
-  }
-
-  const chkPassword = await bcrypt.compare(password, found.password);
-
-  if (!chkPassword) {
-    return res.status(401).json({ message: "Invalid Password" });
-  }
-
-  
-  const token = crypto.randomBytes(16).toString("hex");
-  found.token = token; 
-
-  res.status(200).json({ message: "success", token });
+  if (ckPassword)
+    res.json({
+      message: "login successful",
+      token: jwt.sign(user, secretKey, { expiresIn: "1h" }),
+    });
+  else res.status(401).json({ message: "invalid credentials" });
 });
-
+app.get("/", middleware, (req, res) => {
+  res.json({ message: "welcome to home page" });
+});
 app.get("/users", (req, res) => {
   res.json(users);
-});
-
-app.get("/", auth, (req, res) => {
-  res.send(`Helloooooo worlddddd..... Welcome ${req.user.email}`);
 });
