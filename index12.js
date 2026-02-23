cc23440be90cd0a1e17af292e2494a6acf9f96cd
satyam-ku <rajsatyam2005@gmail.com>
2026-02-23 10:16:50 +0530

    update router

--- index12.js
+++ index12.js
@@ -6,13 +6,23 @@ import { Route } from "lucide-react"
 const app = express()
 app.listen(8080,() => {console.log("Server Started.....")})
 
-const Router = express.Router()
-Router.get("/",(request,response) => {
-    response.send("This is get Request...")
+const userRouter = express.Router()
+const productRouter = express.Router()
+
+userRouter.get("/",(request,response) => {
+    response.send("This is get Request..of users")
 })
 
-Router.post("/",(request,response) => {
-    response.send("This is post Request..")
+userRouter.post("/",(request,response) => {
+    response.send("This is post Request..od users")
 })
 
-app.use("/api/users",Router)
+productRouter.get("/",(request,response) => {
+    response.send("This is get Request..of products")
+})
+
+productRouter.post("/",(request,response) => {
+    response.send("This is post Request..of products")
+})
+app.use("/api/users",userRouter)
+app.use("/api/products",productRouter)
