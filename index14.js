9a9915913c377d38e624ae07e44f435416cfd7c1
satyam-ku <rajsatyam2005@gmail.com>
2026-02-23 13:41:17 +0530

    add create secret from jwt

--- index14.js
+++ index14.js
@@ -1,4 +1,13 @@
 import express from "express"
+import jwt from "jsonwebtoken"
 const app = express()
+const SECRET = "lpu";
 app.listen(8080, () => {console.log("Server Started...")})
 
+const user = {
+    name: "viv",
+    email: "viv@gmail.com",
+    role: "Student"
+}
+
+const token = jwt.sign(user,SECRET, {expiresIn: "1hr"})
